15390fcb52ef4b5be279edb97ffd9c29a2396e28
rhuan <laterhuanme@gmail.com>
2026-05-22 17:27:05 -0300

    feat: adiciona testContext

--- tests/repos.spec.js
+++ tests/repos.spec.js
@@ -2,19 +2,30 @@ const {test, expect}=require('@playwright/test')
 const ServiceFactory = require('../core/serviceFactory');
 const Assertions=require('../core/assertions')
 const testData=require('../fixtures/testData')
+const testContext = require('../core/testContext');
 
 test.describe('Repos API Tests', () => {
     let serviceFactory;
     let assertions;
 
+    test.beforeAll(() => {
+        if (process.env.GITHUB_TOKEN) {
+            testContext.setGlobal('token', process.env.GITHUB_TOKEN);
+        }
+        testContext.setSuite('defaultUser', testData.validUser);
+    });
+
     test.beforeEach(async ({ request }) => {
+        testContext.clearTest();
+
         serviceFactory = new ServiceFactory(request, {
             baseURL: 'https://api.github.com'
         });
         assertions = new Assertions();
 
-        if (process.env.GITHUB_TOKEN) {
-            serviceFactory.setAuthToken(process.env.GITHUB_TOKEN);
+        const token = testContext.getGlobal('token');
+        if (token) {
+            serviceFactory.setAuthToken(token);
         }
     });
 
@@ -23,18 +34,20 @@ test.describe('Repos API Tests', () => {
         const service = serviceFactory.getReposService(request)
         const assertions = new Assertions()
 
-        const response=await service.listRepos(testData.validUser)
+        const user = testContext.getSuite('defaultUser');
+        const response=await service.listRepos(user)
         const body=await response.json()
 
         assertions.assertStatus(response, 200)
-        assertions.assertOwner(body, testData.validUser)
+        assertions.assertOwner(body, user)
     })
 
     test('T011 - Validar estrutura da resposta', async ({ request }) => {
         const service = serviceFactory.getReposService(request)
         const assertions = new Assertions()
 
-        const response=await service.listRepos(testData.validUser)
+        const user = testContext.getSuite('defaultUser');
+        const response=await service.listRepos(user)
 
         assertions.assertStatus(response, 200)
     })
@@ -43,17 +56,18 @@ test.describe('Repos API Tests', () => {
         const service = serviceFactory.getReposService(request)
         const assertions = new Assertions()
 
-        const response=await service.listRepos(testData.invalidUser)
+        testContext.setTest('invalidUserTarget', testData.invalidUser);
+
+        const response=await service.listRepos(testContext.getTest('invalidUserTarget'))
         const body=await response.json()
 
         assertions.assertStatus(response, 404)
         assertions.assertNotFound(body)
     })
 
-    const hasToken = !!process.env.GITHUB_TOKEN;
-
     test('T015 - Listar meus repositórios (autenticado)', async ({ request }) => {
-        test.skip(!hasToken, 'Este teste requer autenticação - configure GITHUB_TOKEN');
+        const hasToken = !!testContext.getGlobal('token');
+        test.skip(!hasToken, 'Este teste requer autenticação - configure a variável de escopo global token');
         const service = serviceFactory.getReposService();
         
         const response = await service.getAuthenticatedRepos();
@@ -65,6 +79,11 @@ test.describe('Repos API Tests', () => {
 
     test.afterEach(() => {
         serviceFactory.cleanup();
+    });
+
+    test.afterAll(() => {
+        // Limpeza dos dados da suite
+        testContext.clearSuite();
     });
 
 });
--- tests/users.spec.js
+++ tests/users.spec.js
@@ -2,26 +2,37 @@ const { test } = require('@playwright/test');
 const ServiceFactory = require('../core/serviceFactory');
 const Assertions = require('../core/assertions');
 const testData = require('../fixtures/testData');
+const testContext = require('../core/testContext');
 
 test.describe('Users API Tests', () => {
     let serviceFactory;
     let assertions;
     
+    test.beforeAll(() => {
+        if (process.env.GITHUB_TOKEN) {
+            testContext.setGlobal('token', process.env.GITHUB_TOKEN);
+        }
+        testContext.setSuite('validUser', testData.validUser);
+    });
+
     test.beforeEach(async ({ request }) => {
+        testContext.clearTest();
         serviceFactory = new ServiceFactory(request, {
             baseURL: 'https://api.github.com'
         });
         assertions = new Assertions();
         
-        if (process.env.GITHUB_TOKEN) {
-            serviceFactory.setAuthToken(process.env.GITHUB_TOKEN);
+        const token = testContext.getGlobal('token');
+        if (token) {
+            serviceFactory.setAuthToken(token);
         }
     });
     
     test('T007 - Validar busca de usuário válido', async () => {
         const usersService = serviceFactory.getUsersService();
         
-        const response = await usersService.getUser(testData.validUser);
+        const user = testContext.getSuite('validUser');
+        const response = await usersService.getUser(user);
         const body = await response.json();
         
         assertions.assertStatus(response, 200);
@@ -42,7 +53,8 @@ test.describe('Users API Tests', () => {
         const service = serviceFactory.getUsersService(request)
         const assertions = new Assertions()
 
-        const response=await service.getUser(testData.validUser)
+        const user = testContext.getSuite('validUser');
+        const response=await service.getUser(user)
         const body=await response.json()
 
         assertions.assertStatus(response, 200)
@@ -51,5 +63,9 @@ test.describe('Users API Tests', () => {
     
     test.afterEach(() => {
         serviceFactory.cleanup();
+    });
+
+    test.afterAll(() => {
+        testContext.clearSuite();
     });
 });
